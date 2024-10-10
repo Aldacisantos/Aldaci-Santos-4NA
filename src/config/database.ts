34b38d4532fbec937424537217aa114ed66d9c05
Aldaci Silva <35945901-aldacisantos9@users.noreply.replit.com>
2024-10-10 00:00:34 +0000

    chore: adicionei helpers e services

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 // Substitua pela sua string de conexão do Render.com
-const connectionString = 'postgresql://aldaci4na_user:xBtvS7STEk2JCBUgGh5zeZBESodG5mga@dpg-crh2o2t6l47c73c1s3sg-a.oregon-postgres.render.com/aldaci4na';
+const connectionString = 'postgresql://backend_ugaz_user:87KlrSzD5GQ0M8KBs5SaJ0bJgGj5112g@dpg-cs2rppbv2p9s738nq950-a.oregon-postgres.render.com/backend_ugaz';
 
 const pool = new Pool({
   connectionString,
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,23 +1,24 @@
 import { Request, Response } from 'express';
-import { UserRepository } from '../repositories/userRepository';
+import pool from '../config/database';
 
-const userRepository = new UserRepository();
-
+// Função para obter todos os usuários
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const users = await userRepository.getAllUsers();
-    res.status(200).json(users);
+    const { rows } = await pool.query('SELECT * FROM users');
+    res.status(200).json(rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Erro ao buscar usuários' });
   }
 };
 
+// Função para adicionar um novo usuário
 export const addUser = async (req: Request, res: Response) => {
   const { name, email } = req.body;
   try {
-    const user = await userRepository.addUser(name, email);
-    res.status(201).json(user);
+    const queryText = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *';
+    const { rows } = await pool.query(queryText, [name, email]);
+    res.status(201).json(rows[0]);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Erro ao adicionar usuário' });
