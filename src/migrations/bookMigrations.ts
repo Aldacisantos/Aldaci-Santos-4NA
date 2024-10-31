764bf1a3d92bb3d559426ba7e2514f53a098a03a
Aldaci Silva <35945901-aldacisantos9@users.noreply.replit.com>
2024-10-31 00:44:53 +0000

    Modularizando com migrations e server

---
+++ src/migrations/bookMigrations.ts
@@ -0,0 +1,24 @@
+import pool from "../config/database";
+
+const createBooksTable = async () => {
+  const book = await pool.connect();
+  try {
+    const queryText = `
+      CREATE TABLE IF NOT EXISTS books (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        author VARCHAR(100) NOT NULL,
+        price DECIMAL (10,2) NOT NULL     
+
+      );
+    `;
+    await book.query(queryText);
+    console.log("Livro cadastrado com sucesso!");
+  } catch (err) {
+    console.error("Erro ao cadastrar livro:", err);
+  } finally {
+    book.release();
+  }
+};
+
+createBooksTable().then(() => process.exit(0));
